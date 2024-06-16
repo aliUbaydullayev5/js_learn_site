5b2c2f10cc50984e32d29a2ed6a2561bd72ad6e8
aliUbaydullayev5 <mukhammadali.ubaydullayev.05@inbox.ru>
2024-06-16 14:49:24 +0500

    update

--- pages/home_section/index.js
+++ pages/home_section/index.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('./pages/header/index.html')
+    fetch('./pages/home_section/index.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header').innerHTML = data;
+            document.getElementById('home_section').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
 });
--- pages/question_area/index.js
+++ pages/question_area/index.js
@@ -1,8 +1,10 @@
 document.addEventListener("DOMContentLoaded", function() {
-    fetch('./pages/header/index.html')
+    fetch('./pages/question_area/index.html')
         .then(response => response.text())
         .then(data => {
-            document.getElementById('header').innerHTML = data;
+            document.getElementById('question_area').innerHTML = data;
         })
         .catch(error => console.error('Error loading header:', error));
 });
+
+let test = 'TEST_EXAMPLE'
